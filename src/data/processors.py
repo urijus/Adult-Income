d16f00539f9da14bad47fcf79159ad0b3da6b351
oriol.juan <oriol.juan@basetis.com>
2024-10-20 20:59:35 +0200

    [NEW] New base and child classe Processor, along with its utils,

---
+++ src/data/processors.py
@@ -0,0 +1,227 @@
+"""This file contains the classes to process the dataset."""
+
+import numpy as np
+import pandas as pd
+from abc import ABC, abstractmethod
+from pandas.api.types import is_numeric_dtype
+from sklearn.model_selection import train_test_split
+
+from utils import TargetTransformer, FeatureTransformer
+
+
+### Base Processor ###
+
+class BaseProcessor():
+    """
+    A base class for loading, preprocessing, and transforming datasets.
+    
+    This class provides a full pipeline for data preparation, including:
+    - Loading data (to be implemented by subclasses)
+    - Handling missing values
+    - Separating features and target
+    - Identifying numerical and categorical features
+    - Encoding and scaling data
+    - Splitting the data into training and testing sets
+    
+    Subclasses must implement the `load_data` method to load the dataset.
+    """
+    def __init__(self, file_path):
+        """
+        Initialize the BaseProcessor with a file path.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the data file that will be loaded for processing.
+
+        Attributes
+        ----------
+        df : pd.DataFrame, optional
+            The loaded dataset.
+        X_train : pd.DataFrame, optional
+            Training set features after data splitting.
+        X_test : pd.DataFrame, optional
+            Test set features after data splitting.
+        y_train : pd.Series or pd.DataFrame, optional
+            Training set target values.
+        y_test : pd.Series or pd.DataFrame, optional
+            Test set target values.
+        num_features : list, optional
+            List of numerical feature column names.
+        cat_features : list, optional
+            List of categorical feature column names.
+        """
+        self.file_path = file_path
+        self.df = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.num_features = None
+        self.cat_features = None
+
+    @abstractmethod
+    def load_data(self, **kwargs):
+        """Abstract method to load data, to be implemented by subclasses."""
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def drop_missing_data(self):
+        """Handle missing values by dropping rows with missing data."""
+        if self.df is not None:
+            self.df = self.df.dropna()
+            print(f'Dropped {np.abs(int(len(self.df) - self.len_original_df ))} missing values.')
+
+    def separate_features_target(self, target_column = None):
+        """Separate features (X) and target (y)."""
+        if target_column is None:
+            target_column = self.df.columns[-1]
+
+        self.X = self.df.drop(columns=[target_column])
+        self.y = self.df[target_column]
+
+    def split_data(self, test_size = 0.3, random_state = 42):
+        """Split the dataset into training and testing sets."""
+        self.X_train, self.X_test, self.y_train, self.y_test, self.y_encoded_train, self.y_encoded_test = train_test_split(
+            self.X, self.y, self.y_encoded, test_size=test_size, random_state=random_state
+        )
+
+        assert len(self.X_train) == len(self.y_train), 'Warning;: X_train and y_train lenghts do not match.'
+        assert len(self.X_test) == len(self.y_test), 'Warning: X_test and y_test lenghts do not match.'
+
+        print(f'Split dataset with ratio {test_size}.')
+
+    def identify_feature_types(self):
+        """Identify numerical and categorical features."""
+        self.num_features = self.X.select_dtypes(include=[float, int]).columns.tolist()
+        self.cat_features = self.X.columns.difference(self.num_features).tolist()
+
+        print(f'Dataset has {len(self.num_features)} numerical features and {len(self.cat_features)} categorical features.')
+
+    def encode_target(self, target_column, encode_type='LabelEncoder'):
+        """Encode the target variable."""
+        encoder = TargetTransformer(self.y, encode_type)
+        self.y = encoder.encode() #output
+
+        label_encoder = TargetTransformer(self.y, 'LabelEncoder')
+        self.y_encoded = encoder.encode() #we'll use this to fit the TargetEncoder of categorical values, need to be 1D numerical array.
+
+        print(f"Encoded the target feature y '{target_column}'.")
+
+    def initialize_transformer(self, num_scaler, cat_encoder, cardinality_threshold, encode_cat, scale_num):
+        """Initialize the FeatureTransformer for feature scaling and encoding."""
+        return FeatureTransformer(
+            num_features=self.num_features,
+            cat_features=self.cat_features,
+            num_scaler=num_scaler,
+            cat_encoder=cat_encoder,
+            cardinality_threshold=cardinality_threshold,
+            X_train=self.X_train,
+            X_test=self.X_test,
+            y_encoded_train=self.y_encoded_train,
+            encode_cat = encode_cat, 
+            scale_num = scale_num
+        )
+
+    def apply_transformations(self, transformer):
+        """Apply transformations to the training and testing sets."""
+        X_train_transformed = transformer.fit_transform()
+        X_test_transformed = transformer.transform()
+
+        return X_train_transformed, X_test_transformed
+
+    def to_dataframe(self, y):
+        if isinstance(y, pd.Series):
+            Y = y.to_frame()
+        elif isinstance(y, np.ndarray):
+            Y = pd.DataFrame(y)
+        else:
+            Y = y
+
+        return Y
+
+    def concatenate_target(self, y, X, target_column):
+        Y = self.to_dataframe(y)
+        
+        # Rename the columns to target_1, target_2, ..., etc.
+        Y.columns = [f"{target_column}_{i+1}" for i in range(Y.shape[1])]
+
+        # Concatenate the features and renamed target columns
+        dataset = pd.concat([X.reset_index(drop=True), Y.reset_index(drop=True)], axis=1)
+
+        return dataset
+
+
+    def preprocess(
+        self, dropna=True, target_column=None, encode_target=True, encode_type='LabelEncoder',
+        test_size=0.3, random_state=42, num_scaler='MinMaxScaler', cat_encoder='TargetEncoder',
+        cardinality_threshold=20, encode_cat = False, scale_num = True
+    ):
+        """
+        Full data preprocessing pipeline:
+        - Handle missing values (optional)
+        - Separate features and target
+        - Identify numerical and categorical features
+        - Encode and scale data
+        - Split into training and testing datasets
+        """
+        # Load data and handle missing values
+        if dropna:
+            self.drop_missing_data()
+
+        # Separate features and target
+        self.separate_features_target(target_column)
+
+        # Encode the target variable if required
+        if encode_target:
+            self.encode_target(target_column, encode_type)
+
+        else:
+            self.y_encoded = self.y
+
+        # Split data into training and testing sets
+        self.split_data(test_size=test_size, random_state=random_state)
+
+        # Identify numerical and categorical features
+        self.identify_feature_types()
+
+        if encode_cat or scale_num:
+            transformer = self.initialize_transformer(num_scaler, cat_encoder, cardinality_threshold, encode_cat, scale_num)
+            X_train, X_test = self.apply_transformations(transformer)
+            print('Apply scaling or encoding.')
+
+        else:
+            X_train, X_test = self.X_train, self.X_test
+            print('No scaling or encoding applied.')
+
+        train_dataset = self.concatenate_target(self.y_train, X_train, target_column = target_column)
+        test_dataset = self.concatenate_target(self.y_test, X_test, target_column = target_column)
+
+        return train_dataset, test_dataset
+        
+
+### Custom Processors ###
+
+class CSVProcessor(BaseProcessor):
+    """
+    A processor for loading and handling CSV files.
+    Inherits from BaseProcessor and implements the load_data method.
+    """
+    def load_data(self, **kwargs):
+        """Load data from a CSV file."""
+        self.df = pd.read_csv(self.file_path, **kwargs)
+        self.len_original_df = len(self.df)
+        print(f'Loaded a CSV file with {self.len_original_df} rows.')
+
+
+
+class ExcelProcessor(BaseProcessor):
+    """
+    A processor for loading and handling Excel files.
+    Inherits from BaseProcessor and implements the load_data method.
+    """
+    def load_data(self, **kwargs):
+        """Load data from an Excel file."""
+        self.df = pd.read_excel(self.file_path, **kwargs)
+        self.len_original_df = len(self.df)
+        print(f'Loaded a Excel file with {self.len_original_df} rows.')
+
---
+++ src/data/utils.py
@@ -0,0 +1,254 @@
+"""This file contains classes for encoding categorical and numerical data."""
+
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import category_encoders as ce
+
+from sklearn.preprocessing import (
+    OneHotEncoder, LabelEncoder, MinMaxScaler, StandardScaler
+)
+
+
+### Preprocessing Classes ###
+
+class TargetTransformer:
+    """
+    A class to handle encoding of target variables (y) using various encoding methods.
+    
+    Supported encoders:
+    - LabelEncoder: Encodes target labels with values between 0 and n_classes-1.
+    - OneHotEncoder: Converts the target variable into one-hot encoded format.
+    - BinaryEncoder: Encodes target labels into binary form (useful for high cardinality features).
+
+    Attributes
+        y : pd.Series or np.array
+            The target variable to encode.
+        encode_type : str
+            The type of encoding to apply. Options are 'LabelEncoder', 'OneHotEncoder', and 'BinaryEncoder'.
+    
+    Methods
+        encode():
+            Encodes the target variable using the specified encoding method.
+        _label_encode():
+            Applies label encoding to the target variable.
+        _onehot_encode():
+            Applies one-hot encoding to the target variable.
+        _binary_encode():
+            Applies binary encoding to the target variable.
+    """
+    ENCODERS = {
+        'LabelEncoder': '_label_encode',
+        'OneHotEncoder': '_onehot_encode',
+        'BinaryEncoder': '_binary_encode'
+    }
+
+    def __init__(self, y: pd.DataFrame, encode_type: str):
+        self.y = y
+        self.encode_type = encode_type
+
+    def encode(self) -> np.ndarray:
+        """Encode the target variable using the specified encoding type."""
+        if self.encode_type not in self.ENCODERS:
+            raise ValueError(f"Unknown encode_type '{self.encode_type}'. Choose from 'LabelEncoder', 'OneHotEncoder', or 'BinaryEncoder'.")
+
+        method = getattr(self, self.ENCODERS[self.encode_type])
+        return method()
+
+    def _label_encode(self):
+        """Apply Label Encoding to the target variable."""
+        return LabelEncoder().fit_transform(self.y)
+    
+    def _onehot_encode(self):
+        """Apply One-Hot Encoding to the target variable."""
+        onehot_encoder = OneHotEncoder(sparse_output=False)
+        return onehot_encoder.fit_transform(self.y.values.reshape(-1, 1))
+
+    def _binary_encode(self):
+        """Apply Binary Encoding to the target variable."""
+        binary_encoder = ce.BinaryEncoder()
+        return binary_encoder.fit_transform(self.y).values
+
+
+class FeatureTransformer:
+    """
+    A class for transforming numerical and categorical features with scalers and encoders.
+    Supports numerical scaling (MinMaxScaler, StandardScaler) and categorical encoding (TargetEncoder or OneHot).
+    """
+
+    NUM_SCALERS = {
+        'MinMaxScaler': MinMaxScaler,
+        'StandardScaler': StandardScaler
+    }
+
+    # For high cardinality categorical features, low cardinality always will be applied one-hot encoding.
+    CAT_ENCODERS = {
+        'TargetEncoder': ce.TargetEncoder  
+    }
+
+    def __init__(self, num_features, cat_features, num_scaler, cat_encoder, cardinality_threshold, X_train, X_test, y_encoded_train, encode_cat, scale_num):
+        """
+        Parameters:
+        ----------
+        num_features : list
+            List of numerical feature column names.
+        cat_features : list
+            List of categorical feature column names.
+        num_scaler : str
+            The scaler type for numerical features ('MinMaxScaler', 'StandardScaler').
+        cat_encoder : str
+            The encoder type for categorical features ('TargetEncoder').
+        cardinality_threshold : int
+            The threshold to differentiate between high and low cardinality categorical features.
+        X_train : pd.DataFrame
+            The training data (features).
+        X_test : pd.DataFrame
+            The testing data (features).
+        y_train : pd.Series or np.array
+            The target variable for training data.
+        """
+
+        self.num_features = num_features
+        self.cat_features = cat_features
+        self.cardinality_threshold = cardinality_threshold
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_encoded_train = y_encoded_train
+        self.encode_cat = encode_cat
+        self.scale_num = scale_num
+
+        # Validate scaler and encoder input and instantiate
+        self.scaler = self._get_scaler(num_scaler)
+        self.encoder = self._get_encoder(cat_encoder)
+
+        # Dictionary to store encoders for high and low cardinality features
+        self.encoders = {}
+
+    def _get_scaler(self, scaler_type):
+        """Helper method to get the numerical scaler."""
+        if scaler_type not in self.NUM_SCALERS:
+            raise ValueError(f"Unknown num_scaler '{scaler_type}'. Choose from {list(self.NUM_SCALERS.keys())}.")
+        return self.NUM_SCALERS[scaler_type]()  # Initialize the scaler
+
+    def _get_encoder(self, encoder_type):
+        """Helper method to get the categorical encoder."""
+        if encoder_type not in self.CAT_ENCODERS:
+            raise ValueError(f"Unknown cat_encoder '{encoder_type}'. Choose from {list(self.CAT_ENCODERS.keys())}.")
+        return self.CAT_ENCODERS[encoder_type]  
+
+    def fit_transform(self):
+        """
+        Fit and transform the training data by encoding categorical features and scaling numerical features.
+
+        Categorical features with high cardinality are target encoded, while low cardinality features are 
+        one-hot encoded. Numerical features are scaled using the specified scaler.
+
+        Returns:
+        --------
+        pd.DataFrame
+            The transformed training data with both numerical and categorical features.
+        """
+        transformed_features = []
+
+        if self.encode_cat:
+            # Process categorical features based on cardinality
+            for feature in self.cat_features:
+                cardinality = self.X_train[feature].nunique()
+
+                if cardinality > self.cardinality_threshold:
+                    # Use defined encoder for high cardinality features
+                    encoder = self.encoder(cols=[feature])
+                    X_train_encoded = encoder.fit_transform(self.X_train[[feature]], self.y_encoded_train)
+                    self.encoders[feature] = encoder
+                else:
+                    onehot_encoder = OneHotEncoder(sparse_output=False, drop='first')
+                
+                    X_train_encoded = pd.DataFrame(
+                        onehot_encoder.fit_transform(self.X_train[[feature]]), 
+                        columns=onehot_encoder.get_feature_names_out([feature])
+                    )
+                    X_train_encoded = X_train_encoded.astype(int) 
+                    self.encoders[feature] = onehot_encoder
+
+                # Append the transformed DataFrame to the list
+                transformed_features.append(X_train_encoded.reset_index())
+
+            # Concatenate all transformed categorical features
+            X_train_transformed_cat = pd.concat(transformed_features, axis=1).reset_index(drop=True)
+
+        if self.scale_num:
+            # Scale numerical features
+            X_train_num = self.scaler.fit_transform(self.X_train[self.num_features])
+            X_train_transformed_num = pd.DataFrame(X_train_num, columns=self.num_features).reset_index(drop=True)
+
+        # Combine encoded categorical and scaled numerical features based on conditions
+        if self.encode_cat and self.scale_num:
+            # Combine both numerical and categorical features
+            X_train_transformed = pd.concat([X_train_transformed_num, X_train_transformed_cat], axis=1).reset_index(drop=True)
+        elif self.encode_cat:
+            # Return only encoded categorical features
+            X_train_transformed = pd.concat([self.X_train[self.num_features].reset_index(drop=True), X_train_transformed_cat], axis=1).reset_index(drop=True)
+        else:
+            # Return only scaled numerical features
+            X_train_transformed = pd.concat([X_train_transformed_num, self.X_train[self.cat_features].reset_index(drop=True)], axis=1).reset_index(drop=True)
+
+        return X_train_transformed
+
+
+    def transform(self):
+        """
+        Transform the test data using the fitted encoders for categorical features 
+        and the fitted scaler for numerical features.
+
+        Returns:
+        --------
+        pd.DataFrame
+            The transformed test data with both numerical and categorical features.
+        """
+        transformed_features = []
+
+        if self.encode_cat:
+            # Process categorical features
+            for feature in self.cat_features:
+                encoder = self.encoders[feature]
+
+                if isinstance(self.encoders[feature], OneHotEncoder):
+                    # One-Hot Encoding for low-cardinality features
+                    X_test_encoded = pd.DataFrame(
+                        encoder.transform(self.X_test[[feature]]), 
+                        columns=encoder.get_feature_names_out([feature])
+                    )
+                else:
+                    # Target Encoding for high-cardinality features
+                    X_test_encoded = encoder.transform(self.X_test[[feature]])
+
+                transformed_features.append(X_test_encoded.reset_index())
+
+            X_test_transformed_cat = pd.concat(transformed_features, axis=1).reset_index(drop=True)
+
+        if self.scale_num:
+            # Scale numerical features
+            X_test_num = self.scaler.transform(self.X_test[self.num_features])
+            X_test_transformed_num = pd.DataFrame(X_test_num, columns=self.num_features)
+
+        # Combine encoded categorical and scaled numerical features based on conditions
+        if self.encode_cat and self.scale_num:
+            # Combine both numerical and categorical features
+            X_test_transformed = pd.concat([X_test_transformed_num, X_test_transformed_cat], axis=1).reset_index(drop=True)
+        elif self.encode_cat:
+            # Return only encoded categorical features
+            X_test_transformed = pd.concat([self.X_test[self.num_features].reset_index(drop=True), X_test_transformed_cat], axis=1).reset_index(drop=True)
+        else:
+            # Return only scaled numerical features
+            X_test_transformed = pd.concat([X_test_transformed_num, self.X_test[self.cat_features].reset_index(drop=True)], axis=1).reset_index(drop=True)
+
+        return X_test_transformed
+
+
+
+
+
+
+
